004a40b631b84beaff0ccb9b48341908b46d7b12
CRAZYRICK\Rick <rhy3htw@gmail.com>
2022-09-25 15:01:22 +0800

    新增功能: 開台通知

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import dotenv
 
 import discord
 
+from discord.ext import tasks
+from twitch.live import is_live
+
 dotenv.load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 JOIN_ID = int(os.getenv('JOIN_ID'))
@@ -12,6 +15,7 @@ VOICE_PORTAL_ID = int(os.getenv('VOICE_PORTAL_ID'))
 VOICE_CHANNEL_ID = int(os.getenv('VOICE_CHANNEL_ID'))
 ROLE_MESSAGE_ID = int(os.getenv('ROLE_MESSAGE_ID'))
 TEST_ROLE_ID = int(os.getenv('TEST_ROLE_ID'))
+NOTIFY_ID = int(os.getenv('NOTIFY_ID'))
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -21,19 +25,53 @@ intents.reactions = True
 
 client = discord.Client(intents=intents)
 
+log_path = './log.txt'
+log_txt = open(log_path, 'r')
+log_lines = []
+for line in log_txt.readlines():
+    log_lines.append(line.replace("\n", ""))
+log_txt.close()
+
+live_status = False
+
+
+@tasks.loop(seconds=60)
+async def live_notify_task():
+    global live_status
+    channelStatus = is_live()
+    
+    if live_status and len(channelStatus) == 0:
+        live_status = False
+    if not live_status and len(channelStatus) == 1 and channelStatus[0]["started_at"] not in log_lines:
+        live_status = True
+        user_login = channelStatus[0]['user_login']
+        user_name = channelStatus[0]['user_name']
+        started_at = f"{channelStatus[0]['started_at']}\n"
+        log_txt = open(log_path, 'a+')
+        log_txt.write(started_at)
+        log_txt.close()
+        log_lines.append(started_at)
+
+        # Discord 通知
+        channel = client.get_channel(NOTIFY_ID)
+        await channel.send(f"https://www.twitch.tv/{user_login} {user_name} 開台了")
+
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'機器人登入囉 {client.user}')
+    live_notify_task.start()
 
     # numberOfMembersChannel = client.get_channel(MEMBER_NUNBER_ID)
     # await numberOfMembersChannel.edit(name=f"人數：{len(client.guilds[0].members)}")
+
 
 @client.event
 async def on_member_join(member):
     channel = client.get_channel(JOIN_ID)
 
     await channel.send(f"{member.mention}")
+
 
 @client.event
 async def on_member_remove(member):
@@ -42,6 +80,8 @@ async def on_member_remove(member):
     await channel.send(f"{member.mention}")
 
 tempVoiceChannels = {}
+
+
 @client.event
 async def on_voice_state_update(member, before, after):
     if before.channel is None and after.channel is not None:
@@ -56,6 +96,7 @@ async def on_voice_state_update(member, before, after):
             if before.channel.id == tempVoiceChannels[member.id]:
                 await before.channel.delete()
                 del tempVoiceChannels[member.id]
+
 
 @client.event
 async def on_raw_reaction_add(payload):
